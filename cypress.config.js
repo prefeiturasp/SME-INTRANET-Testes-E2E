fe8ecf880001280d64625c60c780c1cc7924add7
Wilson Santos <wilson.santos@spassu.com.br>
2025-06-26 12:44:05 -0300

    Inclusão do código para execução no Jenkins

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,25 @@
-const { defineConfig } = require('cypress')
-const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-	e2e: {
-		setupNodeEvents(on, config) {
-			on('file:preprocessor', cucumber())
-		},
-		specPattern: 'cypress/e2e/**/**/*.{feature,cy.{js,jsx,ts,tsx}}',
-		baseUrl: 'https://hom-intranet.sme.prefeitura.sp.gov.br/',
-	},
-})
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Suporte ao Cucumber
+      on('file:preprocessor', cucumber());
+
+      // Suporte ao Allure
+      allureWriter(on, config);
+
+      return config;
+    },
+    specPattern: 'cypress/e2e/**/**/*.{feature,cy.{js,jsx,ts,tsx}}',
+    supportFile: 'cypress/support/e2e.js',
+    baseUrl: 'https://hom-intranet.sme.prefeitura.sp.gov.br/',
+    reporter: 'mocha-allure-reporter',
+    reporterOptions: {
+      overwrite: false,
+      outputDir: 'allure-results',
+    },
+  },
+});
