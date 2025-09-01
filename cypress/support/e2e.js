92ce27d0f0a5711e0fdf256d30a78897b06c100e
marcelolunaspassu <marcelo.jesus@spassu.com.br>
2025-09-01 06:31:51 -0300

    "Adiciona o teste de adicionar uma nova página"

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,8 @@
 import './commands/commands_login'
 import './commands/commands_globais'
 import './commands/commands_noticias'
-import '@shelex/cypress-allure-plugin';
+import './commands/commands_paginas'
+import '@shelex/cypress-allure-plugin'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
 	return false
