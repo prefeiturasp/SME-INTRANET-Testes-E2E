974cb420aa28fe777b36a61802723458894bce76
marcelolunaspassu <marcelo.jesus@spassu.com.br>
2025-09-01 08:56:19 -0300

    "Resolving conflicts"

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,25 @@
-const { defineConfig } = require('cypress');
-const cucumber = require('cypress-cucumber-preprocessor').default;
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { defineConfig } = require('cypress')
+const cucumber = require('cypress-cucumber-preprocessor').default
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // Suporte ao Cucumber
-      on('file:preprocessor', cucumber());
+	e2e: {
+		setupNodeEvents(on, config) {
+			// Suporte ao Cucumber
+			on('file:preprocessor', cucumber())
 
-      // Suporte ao Allure
-      allureWriter(on, config);
+			// Suporte ao Allure
+			allureWriter(on, config)
 
-      return config;
-    },
-    specPattern: 'cypress/e2e/**/**/*.{feature,cy.{js,jsx,ts,tsx}}',
-    supportFile: 'cypress/support/e2e.js',
-    baseUrl: 'https://hom-intranet.sme.prefeitura.sp.gov.br/',
-    reporter: 'mocha-allure-reporter',
-    reporterOptions: {
-      overwrite: false,
-      outputDir: 'allure-results',
-    },
-  },
-});
+			return config
+		},
+		specPattern: 'cypress/e2e/**/**/*.{feature,cy.{js,jsx,ts,tsx}}',
+		supportFile: 'cypress/support/e2e.js',
+		baseUrl: 'https://hom-intranet.sme.prefeitura.sp.gov.br/',
+		reporter: 'mocha-allure-reporter',
+		reporterOptions: {
+			overwrite: false,
+			outputDir: 'allure-results',
+		},
+	},
+})
